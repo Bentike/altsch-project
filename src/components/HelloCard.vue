36eaef13579376f56c1c56e1e6a7806e29d5cccf
bentike <ajadiyusuf73@gmail.com>
2023-03-16 17:12:18 +0100

    successfully displayed all repos

--- src/components/HelloCard.vue
+++ src/components/HelloCard.vue
@@ -1,12 +1,36 @@
 <template>
-  <div></div>
+  <div class="card">
+     <p><span class="title">Fullname:</span> {{ fullname }}</p>
+     <p><span class="title">Url:</span> {{ url }}</p>
+     <p><span class="title">Name:</span> {{ name }}</p>
+     <p><span class="title">ID:</span> {{ id }}</p>
+     <p><span class="title">Default Branch:</span> {{ branch }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HelloCard",
-  props: {
-    name: String,
-  },
+  props: ["fullname", "url", "name", "id", "branch"]
 };
 </script>
+
+<!-- <style>
+ .card{
+  width: 40%;
+  border-radius: 15px;
+  background-color: red;
+  padding: 20px;
+  transition: all ease-in-out .2s;
+ }
+ .card:hover{
+  transform: scale(1.1); 
+ }
+ .title{
+  color: #000;
+  font-style: italic;
+ }
+ p{
+  color:#fff
+ }
+</style> -->
