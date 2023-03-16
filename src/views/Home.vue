809f40c749a540807e55409127ad14910414083c
bentike <ajadiyusuf73@gmail.com>
2023-03-16 14:31:43 +0100

    added Home.vue and Repo.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>This is the Home page</h1>
+        <p>All Repos will be Displayed here...</p>
+    </div>
+</template>
---
+++ src/views/Repo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>A single Repo will be Displayed here...</h1>
+    </div>
+</template>
