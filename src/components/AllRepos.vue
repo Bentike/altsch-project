1987fa2fdedfdeaf0b1c04cd6f08c812801e1a13
bentike <ajadiyusuf73@gmail.com>
2023-03-16 14:28:42 +0100

    added views folder

--- src/components/AllRepos.vue
+++ src/components/AllRepos.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
         <h1>All My Repos</h1>
+        <p>{{ repos }}</p>
+        <button @click="setAllRepos">change repo</button>
     </div>
 </template>
 
 <script>
  export default{
-    name: 'AllRepos'
+    name: 'AllRepos',
+    data() {
+        return{
+            repos: []
+        }
+    },
+    methods: {
+        setAllRepos(){
+            this.repos = "Bentee";
+        }
+    },
+    mounted(){
+       fetch("https://api.github.com/users/bentike/repos")
+       .then(res => res.json())
+       .then(data => {
+         this.repos = data;
+       });
+    }
  }
 </script>
