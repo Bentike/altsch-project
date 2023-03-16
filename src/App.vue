16868d5915021a23b33f48772366bb86549e9506
bentike <ajadiyusuf73@gmail.com>
2023-03-16 11:19:44 +0100

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome To My Alt School Final Project"/>
+  <HelloCard name="Bentike"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloCard from './components/HelloCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloCard
   }
 }
 </script>
---
+++ src/components/HelloCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello {{ name }}</h1>
+        <h3>
+            Repo Cards will be here.
+        </h3>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloCard',
+    props: {
+        name: String
+    }
+  }
+</script>
