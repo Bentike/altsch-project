1528adc1f7255c7b192c2ea75fa456b13d6beeea
bentike <ajadiyusuf73@gmail.com>
2023-03-18 12:13:25 +0100

    added NotFound page to views

---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <p>Oppsss!</p>
+        <p>The page you are looking for is not available</p>
+        <router-link to="/">Go back Home</router-link>
+    </div>
+</template>
