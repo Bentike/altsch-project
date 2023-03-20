ecc0084c08c4f2a75567910cd2ad6d5d911c762a
bentike <ajadiyusuf73@gmail.com>
2023-03-20 15:37:42 +0100

    formatted NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,13 +1,19 @@
 <template>
+  <div>
+    <img
+      class="error-img"
+      src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg"
+      alt="404 error"
+    />
     <div>
-        <img class="error-img" src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="404 error"/>
-        <router-link to="/">Go back Home</router-link>
+      <router-link to="/">Go back Home</router-link>
     </div>
+  </div>
 </template>
 
 <style>
-.error-img{
-    width: 300px;
-    height: 300px;
+.error-img {
+  width: 300px;
+  height: 300px;
 }
 </style>
